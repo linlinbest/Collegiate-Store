511754716c64ceff102b56e2cbe23e635bc5e915
Jeremiah C. Shikan <github.shikanchen@gmail.com>
2018-11-27 11:25:22 -0500

    move in 360 mode functionality added

--- View/javascripts/index.js
+++ View/javascripts/index.js
@@ -6,7 +6,30 @@ $(document).ready(function() {
     window.open("../Controller/index.controller.php?location="+localStorage.getItem("location"));
   });
 
+  if (localStorage.getItem("position") != null) {
+    $('#myblock').attr('src', '../Data/'+localStorage.getItem("position")+'.jpg')
+  }
+
   $('#myblock').draggable({
     appendTo: "#canvas"
   });
+
+  $('.up').click(function() {
+    if (localStorage.getItem("position") === null) {
+      localStorage.setItem("position", "010");
+    }
+    num = parseInt(localStorage.getItem("position"), 10) + 10;
+    num = num + "";
+    for (var i = 0; i < 3 - num.length; i++) {
+      num = "0" + num;
+    }
+    localStorage.setItem("position", num);
+
+    $('#myblock').attr('src', '../Data/'+localStorage.getItem("position")+'.jpg')
+
+    alert(localStorage.getItem("position"))
+  })
+
+  // DEBUG MODE only
+  // localStorage.clear();
 });
