82ef7a692755d761057b150e6833d1bf8c59d7cd
linlinbest <43051929+linlinbest@users.noreply.github.com>
2018-12-10 00:04:54 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,132 @@
+//preload the images
+function preload(arr_images) {
+  var newimages = [];
+  for (var i = 0; i < arr_images.length; i++) {
+    newimages[i] = new Image();
+    newimages[i].src = arr_images[i];
+  }
+}
+var imgs = ['../Data/002.jpg', '../Data/003.jpg', '../Data/010.jpg', '../Data/100.jpg', '../Data/020.jpg', '../Data/030.jpg', '../Data/200.jpg', '../Data/300.jpg'];
+preload(imgs);
+
+$(document).ready(function() {
+  $('.enter').click(function() {
+    if (localStorage.getItem("location") === null) {
+      localStorage.setItem("location", "init");
+    }
+    // window.open("../Controller/index.controller.php?location="+localStorage.getItem("location"));
+  });
+
+  if (localStorage.getItem("position") != null) {
+    $('#myblock').attr('src', '../Data/'+localStorage.getItem("position")+'.jpg')
+  }
+
+  $('#myblock').draggable({
+    axis: 'x',
+    drag: function( event, ui ) {
+    // Setting limits
+    if (ui.position.left > 800) {
+      ui.position.left = 800;
+    }
+    if (ui.position.left < -1000) {
+      ui.position.left = -1000
+    }
+  }
+  });
+
+
+  // up event listener
+  $('.up').click(function() {
+    $('#myblock').hide();
+    if (localStorage.getItem("position") === null) {
+      localStorage.setItem("position", "010");
+    }
+    raw = localStorage.getItem("position");
+    inc = 1;
+    i = 2;
+    while (raw.charAt(i) == "0" && i >= 0) {
+      inc *= 10;
+      i--;
+    }
+    num = parseInt(raw, 10) + inc;
+    num = num + "";
+    for (var i = 0; i < 3 - num.length; i++) {
+      num = "0" + num;
+    }
+    localStorage.setItem("position", num);
+
+    $('#myblock').attr('src', '../Data/'+localStorage.getItem("position")+'.jpg');
+
+    $('#myblock').fadeIn(1000)
+  })
+
+  // down event listener
+  $('.down').click(function() {
+    $('#myblock').hide();
+    if (localStorage.getItem("position") === null) {
+      localStorage.setItem("position", "010");
+    }
+    raw = localStorage.getItem("position");
+    inc = 1;
+    i = 2;
+    while (raw.charAt(i) == "0" && i >= 0) {
+      inc *= 10;
+      i--;
+    }
+    num = parseInt(raw, 10) - inc;
+    num = num + "";
+    for (var i = 0; i < 3 - num.length; i++) {
+      num = "0" + num;
+    }
+    localStorage.setItem("position", num);
+
+    $('#myblock').attr('src', '../Data/'+localStorage.getItem("position")+'.jpg');
+
+    $('#myblock').fadeIn(1000)
+  })
+
+  // left event listener
+  $('.left').click(function() {
+    $('#myblock').hide();
+    if (localStorage.getItem("position") === null) {
+      localStorage.setItem("position", "010");
+    }
+    num = "";
+    raw = localStorage.getItem("position");
+    console.log(raw)
+    tmp = raw.charAt(0);
+    for (var i = 0; i < raw.length - 1; i++) {
+      num += raw.charAt(i+1);
+    }
+    num += tmp;
+    console.log(num)
+    localStorage.setItem("position", num);
+
+    $('#myblock').attr('src', '../Data/'+localStorage.getItem("position")+'.jpg');
+
+    $('#myblock').fadeIn(1000)
+  })
+
+  // right event listener
+  $('.right').click(function() {
+    $('#myblock').hide();
+    if (localStorage.getItem("position") === null) {
+      localStorage.setItem("position", "010");
+    }
+    raw = localStorage.getItem("position");
+    console.log(raw)
+    num = "" + raw.charAt(raw.length - 1);
+    for (var i = 0; i < raw.length - 1; i++) {
+      num += raw.charAt(i);
+    }
+    console.log(num)
+    localStorage.setItem("position", num);
+
+    $('#myblock').attr('src', '../Data/'+localStorage.getItem("position")+'.jpg');
+
+    $('#myblock').fadeIn(1000)
+  })
+
+  // DEBUG MODE only
+  localStorage.clear();
+});
